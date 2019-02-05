60a8e23a75f741d6eee968cd9df9445d5282b344
Oscar Slater Jimenez Paula <oscar_jimenez@ucne.edu.do>
2019-02-05 15:00:41 -0400

    cambios master y programando registro de cuentas

--- PrimerParcial/Registros/rCuentas.aspx
+++ PrimerParcial/Registros/rCuentas.aspx
@@ -1,7 +1,62 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="rCuentas.aspx.cs" Inherits="PrimerParcial.Registros.rCuentas" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+     <h2 style="color: #000">Registro de Cuentas</h2>
+    <div class="form-row">
+        <%--CuentaId--%>
+        <div class="form-group col-md-3">
+            <asp:Label Text="Cuenta Id" class="text-primary" runat="server" />
+            <asp:TextBox ID="CuentaIdTextBox" class="form-control input-group" TextMode="Number" placeholder="0" runat="server" />
+        </div>
+        <%--Fecha--%>
+        <div class="form-group col-md-3">
+            <asp:Label Text="Fecha" runat="server" />
+            <asp:TextBox ID="FechaTextBox" class="form-control input-group" TextMode="Date" runat="server" />
+        </div>
+
+        <%--Boton--%>
+        <div class="col-lg-1 p-0">
+            <asp:LinkButton ID="BuscarLinkButton" CssClass="btn btn-outline-info mt-4" runat="server" OnClick="BuscarLinkButton_Click">
+                <span class="fas fa-search"></span>
+                     Buscar
+            </asp:LinkButton>
+        </div>
+
+    </div>
+
+    <div class="form-row">
+        <%--Nombre--%>
+        <div class="form-group col-md-3">
+            <asp:Label Text="Nombre" runat="server" />
+            <asp:TextBox ID="NombreTextBox" class="form-control input-sm" runat="server" />
+
+
+        </div>
+
+        <%--Balance--%>
+        <div class="form-group col-md-3">
+            <asp:Label Text="Balance" runat="server" />
+            <asp:TextBox ID="BalanceTextBox" TextMode="Number" ReadOnly="true" class="form-control input-sm" placeholder="0" runat="server" />
+        </div>
+
+    </div>
+
+    <asp:Label ID="MensajeLabel" runat="server" Text=""></asp:Label>
+
 </asp:Content>
 <asp:Content ID="Content3" ContentPlaceHolderID="MainContent" runat="server">
+    <%--Botones--%>
+    <div class="panel-footer">
+        <div class="text-center">
+            <div class="form-group" style="display: inline-block">
+                <asp:Button Text="Nuevo" class="btn btn-outline-info btn-md" runat="server" ID="NuevoButton" OnClick="NuevoButton_Click" />
+                <asp:Button Text="Guardar" class="btn btn-outline-success btn-md" runat="server" ID="GuadarButton" OnClick="GuadarButton_Click"  />
+                <asp:Button Text="Eliminar" class="btn btn-outline-danger btn-md" runat="server" ID="EliminarButton" OnClick="EliminarButton_Click"/>
+
+            </div>
+        </div>
+    </div>
 </asp:Content>
--- PrimerParcial/Registros/rCuentas.aspx.cs
+++ PrimerParcial/Registros/rCuentas.aspx.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BLL;
+using Entities;
+using PrimerParcial.Utilitarios;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +14,112 @@ namespace PrimerParcial.Registros
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            FechaTextBox.Text = DateTime.Now.ToString("yyyy-MM-dd");
+            BalanceTextBox.Text = "0";
+        }
+
+        private void LimpiarCampos()
+        {
+            CuentaIdTextBox.Text = "0";
+            FechaTextBox.Text = DateTime.Now.ToString("yyyy-MM-dd");
+            NombreTextBox.Text = "";
+            BalanceTextBox.Text = "0";
+        }
+
+        private Cuenta LlenaClase()
+        {
+            Cuenta cb = new Cuenta();
+
+            cb.CuentaID = Utils.ToInt(CuentaIdTextBox.Text);
+            cb.Fecha = Convert.ToDateTime(FechaTextBox.Text).Date;
+            cb.Nombre = NombreTextBox.Text;
+            cb.Balance = Utils.ToInt(BalanceTextBox.Text);
+
+            return cb;
 
         }
+
+        protected void BuscarLinkButton_Click(object sender, EventArgs e)
+        {
+            RepositorioBase<Cuenta> repositoriobase = new RepositorioBase<Cuenta>();
+            Cuenta cuentasbancarias = repositoriobase.Buscar(Utils.ToInt(CuentaIdTextBox.Text));
+            if (cuentasbancarias != null)
+            {
+                FechaTextBox.Text = cuentasbancarias.Fecha.ToString();
+                NombreTextBox.Text = cuentasbancarias.Nombre;
+                BalanceTextBox.Text = cuentasbancarias.Balance.ToString();
+            }
+            else
+            {
+                Response.Write("<script>alert('Usuario no encontrado');</script>");
+            }
+        }
+
+        protected void NuevoButton_Click(object sender, EventArgs e)
+        {
+            LimpiarCampos();
+        }
+
+        protected void GuadarButton_Click(object sender, EventArgs e)
+        {
+            RepositorioBase<Cuenta> repositorio = new RepositorioBase<Cuenta>();
+            Cuenta cuentasbancarias = new Cuenta();
+            bool paso = false;
+
+            //todo: validaciones adicionales
+            cuentasbancarias = LlenaClase();
+
+            if (cuentasbancarias.CuentaID == 0)
+            {
+                paso = repositorio.Guardar(cuentasbancarias);
+                Response.Write("<script>alert('Guardado');</script>");
+                LimpiarCampos();
+            }
+            else
+            {
+                Cuenta user = new Cuenta();
+                int id = Utils.ToInt(CuentaIdTextBox.Text);
+                RepositorioBase<Cuenta> repository = new RepositorioBase<Cuenta>();
+                cuentasbancarias = repository.Buscar(id);
+
+                if (user != null)
+                {
+                    paso = repositorio.Modificar(LlenaClase());
+                    Response.Write("<script>alert('Modificado');</script>");
+                }
+                else
+                    Response.Write("<script>alert('Id no existe');</script>");
+            }
+
+            if (paso)
+            {
+                LimpiarCampos();
+            }
+            else
+                Response.Write("<script>alert('No se pudo guardar');</script>");
+
+        }
+
+        protected void EliminarButton_Click(object sender, EventArgs e)
+        {
+            RepositorioBase<Cuenta> repositorio = new RepositorioBase<Cuenta>();
+            int id = Utils.ToInt(CuentaIdTextBox.Text);
+
+            var CuentasBancarias = repositorio.Buscar(id);
+
+            if (CuentasBancarias != null)
+            {
+                if (repositorio.Eliminar(id))
+                {
+                    Response.Write("<script>alert('Eliminado');</script>");
+                    LimpiarCampos();
+                }
+                else
+                    Response.Write("<script>alert('No se pudo eliminar');</script>");
+            }
+            else
+                Response.Write("<script>alert('No existe');</script>");
+        }
     }
-}
+    }
+
