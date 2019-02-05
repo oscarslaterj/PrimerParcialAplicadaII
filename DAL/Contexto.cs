e4a95ffa11955574e78f5e8d142ef48ce8113f35
Oscar Slater Jimenez Paula <oscar_jimenez@ucne.edu.do>
2019-02-05 08:42:28 -0400

    Creando BLL, DAL y Entidades

---
+++ DAL/Contexto.cs
@@ -0,0 +1,22 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Cuenta> Cuenta { get; set; }
+        public DbSet<Deposito> Deposito { get; set; }
+
+        public Contexto() : base("ConStr")
+        {
+
+        }
+
+    }
+}
---
+++ Entities/Deposito.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Deposito
+    {
+        [Key]
+        public int DepositoID { get; set; }
+        public int CuentaID { get; set; }
+        public string Concepto { get; set; }
+        public decimal Monto { get; set; }
+        public DateTime Fecha { get; set; }
+
+        public Deposito()
+        {
+            DepositoID = 0;
+            CuentaID = 0;
+            Concepto = string.Empty;
+            Monto = 0;
+            Fecha = DateTime.Now;
+        }
+    }
+}
+
